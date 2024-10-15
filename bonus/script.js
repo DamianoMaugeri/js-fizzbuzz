29e1a2e3190ba478dc1e8ce47773e5cfd0e0e491
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-10-15 17:27:23 +0200

    [add ] sequenza di fibonacci ( l'avevo fatta infinita ma è esploso il pc )

---
+++ bonus/script.js
@@ -0,0 +1,18 @@
+console.log('fibonacci')
+
+let test = '';
+let nump = 0
+let nums =1
+
+console.log(nump)
+console.log(nums)
+
+for ( let i=0 ; i < 1000; i++){
+
+let numstamp= nump + nums
+console.log(numstamp)
+
+nump = nums
+nums = numstamp
+
+}
