94a86efa3a935bbc95db732c97102d542e3bc017
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-10-16 10:02:27 +0200

    [fix] output

--- script/app.js
+++ script/app.js
@@ -5,19 +5,20 @@ for( let i=0 ; i<100 ; i++){
     let numeri = i + 1
     let modulo3 = numeri % 3
     let modulo5 = numeri % 5
+    let mess = numeri
 
 
     if  (modulo3 === 0 && modulo5 === 0){
 
-        console.log(`FizzBuzz`)
+        mess = 'FizzBuzz'
 
     } else if (modulo3 === 0 ) {
-        console.log(`Fizz`)
+        mess = 'Fizz'
     } else if (modulo5===0){
-        console.log(`Buzz`)
-    } else{
-        console.log(numeri)
-    }
+        mess = 'Buzz'
+    } 
+
+    console.log(mess)
 
 
 }
